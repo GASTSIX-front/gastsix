026ca8ebb04c117cbacc9c042cde237746daeaa3
Edgard Freitas <freitas.edgard@hotmail.com>
2023-07-04 20:33:32 -0300

    Relacionamento de Links e Navegacao

---
+++ log_in/login.php
@@ -0,0 +1,10 @@
+<?php
+$nome = $_POST['name'];
+$email = $_POST['email'];
+
+// Faça o processamento dos dados aqui...
+
+// Exemplo de exibição dos dados enviados
+echo "Nome: " . $nome . "<br>";
+echo "Email: " . $email . "<br>";
+?>
